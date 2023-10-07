7b8df4c995d27dfad370572cc3e7305115ea3bf0
Madhumitha <madhumithavenu03@gmail.com>
2023-10-07 14:28:26 +0530

    Creating blog page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 // const mysql = require('mysql2');
 // const db = require('./config/db.js')
-// const blogRouter = require("./routes/blog-routes.js");
+const blogRouter = require("./routes/blog-routes.js");
 const userRouter = require("./routes/user-routes.js");
 
 // dotenv.config({path: './.env'});
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/user", userRouter);
-// app.use("/api/blog",blogRouter);
+app.use("/api/blog",blogRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=>console.log(`Sever Running on PORT ${PORT}`));
