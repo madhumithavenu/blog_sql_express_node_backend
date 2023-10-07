2fa8358eff2d922a43bf80fb52b8061e6a63169d
Madhumitha <madhumithavenu03@gmail.com>
2023-10-07 14:23:36 +0530

    Adding port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-// app.use("/api/user", userRouter);
+app.use("/api/user", userRouter);
 // app.use("/api/blog",blogRouter);
 
 const PORT = process.env.PORT || 4000;
