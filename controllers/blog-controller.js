0f99279ce9c5db673e5f2e0a0acf725bef643701
Madhumitha <madhumithavenu03@gmail.com>
2023-10-07 15:35:54 +0530

    function to updateblog

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -35,72 +35,67 @@ module.exports.addBlog = async (req, res, next) => {
     })
 };
 
-
-// module.exports.updateBlog = async (req, res, next) => {
-//     const { title, description } = req.body;
-//     const blogId = req.params.id;
-//     let blog;
-//     try {
-//         blog = await Blog.findByIdAndUpdate(blogId, {
-//             "title": title,
-//             "description":description,
-//         });
-//     } catch (err) {
-//         return console.log(err);
-//     }
-//     if (!blog) {
-//         return res.status(500).json({ message: "Unable To Update The Blog" });
-//     }
-//     return res.status(200).json({ blog });
-// };
-
-
-// module.exports.getById = async (req, res, next) => {
-//     const id = req.params.id;
-//     let blog;
-//     try {
-//         blog = await Blog.findById(id).populate('user');
-//     } catch (err) {
-//         return console.log(err);
-//     }
-//     if (!blog) {
-//         return res.status(404).json({ message: "No Blog Found" });
-//     }
-//     return res.status(200).json({ blog });
-// };
+module.exports.updateBlog = async (req, res, next) => {
+    const { title, description,image,id} = req.body;
+    const blogId = req.params.id;
+    db.query('UPDATE BLOGS SET TITLE=?, DESCRIPTION = ?, IMAGE= ?, ID=? WHERE B_ID =?',[title,description,image,id,blogId],(err, results)=>{
+        if (err) {
+            console.log(err);
+            return res.json({
+                message: 'unable to update the Blog'
+            })
+        } else {
+            console.log(results);
+            return res.json({
+                message: 'Blog updated'
+            })
+        }
+    })
+};
 
 
-// module.exports.deleteBlog = async (req, res, next) => {
-//     const id = req.params.id;
-
-//     let blog;
-//     try {
-//         const session = await mongoose.startSession();
-//         session.startTransaction({ session });
-//         blog = await Blog.findByIdAndRemove(id).populate("user");
-//         await blog.user.blogs.pull(blog);
-//         await blog.user.save({ session });
-//         await session.commitTransaction();
-//     } catch (err) {
-//         console.log(err);
-//     }
-//     if (!blog) {
-//         return res.status(500).json({ message: "Unable To Delete" });
-//     }
-//     return res.status(200).json({ message: "Successfully Delete" });
-// };
+module.exports.getById = async (req, res, next) => {
+    const blogId = req.params.id;
+    db.query('SELECT * FROM BLOGS WHERE B_ID=?',[blogId],(err, results)=>{
+        if (err) {
+            console.log(err);
+            return res.json({
+                message: 'unable to find the Blog'
+            })
+        } else {
+            return res.send(results);
+        }
+    })
+};
 
 
-// module.exports.getByUserId = async (req, res, next) => {
-//     const userId = req.params.id;
-//     let userBlogs;
-//     try {
-//         userBlogs = await User.findById(userId).populate("blogs");
-//     } catch (err) {
-//         return console.log(err);
-//     }
-//     if (!userBlogs) {
-//         return res.status(404).json({ message: "No Blog Found" });
-//     }
-//     return res.status(200).json({ user: userBlogs });
-// };
+module.exports.deleteBlog = async (req, res, next) => {
+    const blogId = req.params.id;
+    db.query('DELETE FROM BLOGS WHERE B_ID=?',[blogId],(err, results)=>{
+        if (err) {
+            console.log(err);
+            return res.json({
+                message: 'unable to delete the Blog'
+            })
+        } else {
+            return res.json({
+                message: 'Deleted Successfully'
+            })
+        }
+    })
+};
+
+
+module.exports.getByUserId = async (req, res, next) => {
+    const userId = req.params.id;
+    db.query('SELECT * FROM BLOGS WHERE ID=?',[userId],(err, results)=>{
+        if (err) {
+            console.log(err);
+            return res.json({
+                message: 'unable to find the Blog'
+            })
+        } else {
+            return res.send(results);
+        }
+    })
+};
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -6,11 +6,11 @@ const blogRouter = express.Router();
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlog);
 
-// blogRouter.put("/update/:id", updateBlog);
+blogRouter.put("/update/:id", updateBlog);
 
-// blogRouter.get("/:id", getById);
+blogRouter.get("/:id", getById);
 
-// blogRouter.delete("/:id", deleteBlog);
-// blogRouter.get("/user/:id", getByUserId);
+blogRouter.delete("/:id", deleteBlog);
+blogRouter.get("/user/:id", getByUserId);
 
 module.exports = blogRouter;
