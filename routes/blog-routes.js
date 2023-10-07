66f3b819ba505f44e855a8e306f8f1aaaa36dd80
Madhumitha <madhumithavenu03@gmail.com>
2023-10-07 14:29:03 +0530

    Exporting blog router

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,10 +1,4 @@
 const express = require('express');
-// const getAllBlogs = require("../controllers/blog-controller.js");
-// const addBlog = require("../controllers/blog-controller.js");
-// const updateBlog = require("../controllers/blog-controller.js");
-// const getById = require("../controllers/blog-controller.js");
-// const deleteBlog = require("../controllers/blog-controller.js");
-// const getByUserId = require("../controllers/blog-controller.js");
 const { getAllBlogs, addBlog, updateBlog, getById, deleteBlog, getByUserId } = require('../controllers/blog-controller.js');
 
 const blogRouter = express.Router();
